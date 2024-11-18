bf7c7271a2d15a32d7887c784285cd598a6796d3
Sujin Pongchababnapa <127624492+Jintpong@users.noreply.github.com>
2024-11-18 09:43:41 +0000

    change to airport table

--- main.py
+++ main.py
@@ -7,57 +7,72 @@ def connect_db():
 
 
 #This function is use to add flight
-def add_flight(flight_id, destination_id, departure_date, departure_time, status, pilot_id):
+def add_flight( origin_airport_id, destination_airport_id, departure_date, departure_time,arrival_time, status, pilot_id):
     connect = connect_db()
     cursor = connect.cursor()
-    cursor.execute("INSERT INTO flights (flight_id, destination_id, departure_date, departure_time, status, pilot_id) VALUES (?, ?, ?, ?, ?, ?)",
-                   (flight_id, destination_id, departure_date, departure_time, status, pilot_id))
+
+
+    cursor.execute("INSERT INTO flights (origin_airport_id, destination_airport_id, departure_date, departure_time, arrival_time ,status, pilot_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                   (origin_airport_id, destination_airport_id, departure_date, departure_time, arrival_time, status, pilot_id))
     connect.commit()
+    print("Flight has been added")
     connect.close()
 
-# Need to fix this function since it is not updating the updated flight
-def update_flight_information(flight_id=None, new_flight_id=None, destination_id=None, departure_date=None, 
-                              departure_time=None, status=None, pilot_id=None):
+
+def update_flight_information():
     connect = connect_db()
     cursor = connect.cursor()
+
+    # Prompt for the flight ID to update
+    flight_id = input("Enter the flight ID to update: ").strip()
+
     update_version = []
     value = []
 
-    new_flight_id = input("Enter a new flight id: ").strip()
-    new_destination_id = input("Enter a new destination id: ").strip()
-    new_departure_date = input("Enter a departure date: ").strip()
-    new_departure_time = input("Enter a new departure time: ").strip()
-    new_status = input("Enter a status: ").strip()
-    new_pilot_id = input("Enter a new pilot id : ").strip()
+    # Prompt user for new values
+    new_origin_airport_id = input("Enter a new origin airport ID : ").strip()
+    new_destination_airport_id = input("Enter a new destination airport ID : ").strip()
+    new_departure_date = input("Enter a new departure date (YYYY-MM-DD) ): ").strip()
+    new_departure_time = input("Enter a new departure time (HH:MM) : ").strip()
+    new_arrival_time = input("Enter a new arrival time (HH:MM) : ").strip()
+    new_status = input("Enter a new status : ").strip()
+    new_pilot_id = input("Enter a new pilot ID : ").strip()
 
-    if new_flight_id:
-        update_version.append("flight_id = ?")
-        value.append(new_flight_id)
-    if new_destination_id:
-        update_version.append("destination_id = ?")
-        value.append(destination_id)
+    # Check each input and append the corresponding SQL clause and value
+    if new_origin_airport_id:
+        update_version.append("origin_airport_id = ?")
+        value.append(new_origin_airport_id)
+    if new_destination_airport_id:
+        update_version.append("destination_airport_id = ?")
+        value.append(new_destination_airport_id)
     if new_departure_date:
         update_version.append("departure_date = ?")
-        value.append(departure_date)
+        value.append(new_departure_date)
     if new_departure_time:
         update_version.append("departure_time = ?")
-        value.append(departure_time)
+        value.append(new_departure_time)
+    if new_arrival_time:
+        update_version.append("arrival_time = ?")
+        value.append(new_arrival_time)
     if new_status:
         update_version.append("status = ?")
-        value.append(status)
+        value.append(new_status)
     if new_pilot_id:
         update_version.append("pilot_id = ?")
-        value.append(pilot_id)
+        value.append(new_pilot_id)
+
 
     if update_version:
-        value.append(flight_id)
+        value.append(flight_id)  
         cursor.execute(f"UPDATE flights SET {', '.join(update_version)} WHERE flight_id = ?", value)
         connect.commit()
-        print("The flight has been update")
+        print("The flight has been updated.")
     else:
-        print("The flight has not been update")
+        print("The flight was not update")
 
     connect.close()
+
+
 
 def assign_pilot():
     flight_id = input("Enter the flight id you want to assign the pilot: ").strip()
@@ -96,10 +111,10 @@ def pilot_schedule():
     pilot_id = input("Enter the pilot id to check the schedule: ").strip()
     connect = connect_db()
     cursor = connect.cursor()
-    cursor.execute("SELECT flight_id, destination_id, departure_date, departure_time, status FROM flights WHERE pilot_id = ? ORDER BY departure_date, departure_time", (pilot_id,))
+    cursor.execute("SELECT flight_id, origin_airport_id, destination_airport_id, departure_date, departure_time, arrival_time, status FROM flights WHERE pilot_id = ? ORDER BY departure_date, departure_time", (pilot_id,))
     schedule = cursor.fetchall()
     for flight in schedule:
-        print(f"Flight ID: {flight[0]}, Destination ID: {flight[1]}, Departure Date: {flight[2]}, Departure Time: {flight[3]}, Status: {flight[4]}")
+        print(f"Flight ID: {flight[0]}, Origin Airport ID: {flight[1]},Destination Aiport ID {flight[2]},  Departure Date: {flight[3]}, Departure Time: {flight[4]}, Arrival Time: {flight[5]}, Status: {flight[6]}")
 
     connect.close()
 
@@ -110,23 +125,26 @@ def view_update_destination():
     cursor = connect.cursor()
 
     if user == 'v':
-        destination_id = input("Enter the destination id: ")
-        cursor.execute("SELECT * FROM destinations WHERE destination_id = ?", destination_id,)
+        destination_airport_id = input("Enter the destination airport id: ")
+        cursor.execute("SELECT * FROM airports WHERE airport_id = ?", destination_airport_id,)
         destination_info = cursor.fetchone()
 
-        print(f"Destination Information ID: {destination_id}")
-        print(f"Destination Name: {destination_info[1]}")
-        print(f"Country: {destination_info[2]}")
-        print(f"Destination Airport Code: {destination_info[3]}")
+        if destination_info:
+            print(f"Airport ID: {destination_info[0]}")
+            print(f"Destination Name: {destination_info[1]}")
+            print(f"Country: {destination_info[2]}")
+            print(f"Destination Airport Code: {destination_info[3]}")
+        else:
+            print("No destination airport ID was found")
 
     elif user == 'u':
-        destination_id = input("Enter the Destination ID to update: ").strip()
-        cursor.execute("SELECT * FROM destinations WHERE destination_id = ?", (destination_id,))
+        destination_airport_id = input("Enter the Destination airport ID to update: ").lower().strip()
+        cursor.execute("SELECT * FROM airports WHERE airport_id = ?", (destination_airport_id,))
         destination = cursor.fetchone()
 
-        new_name = input("Enter the new name: ")
-        new_country = input("Enter the new country: ")
-        new_code = input("Enter the new code: ")
+        new_name = input("Enter the new city name: ").strip()
+        new_country = input("Enter the new country: ").strip()
+        new_code = input("Enter the new code: ").strip()
 
         update = []
         value = []
@@ -143,13 +161,41 @@ def view_update_destination():
 
 
         if update:
-            value.append(destination_id) 
-            cursor.execute(f"UPDATE destinations SET {', '.join(update)} WHERE destination_id = ?", value)
+            value.append(destination_airport_id) 
+            cursor.execute(f"UPDATE airports SET {', '.join(update)} WHERE airport_id = ?", value)
             connect.commit()
+            print("The destination has been updated")
         else:
             print("No changes occured")
 
         connect.close()
+
+def flight_by_criteria():
+    connect = connect_db()
+    cursor = connect.cursor()
+    flight_information = ['flight_id', 'origin_airport_id','destination_airport_id', 'departure_date', 'departure_time','arrival_time', 'status', 'pilot_id']
+    print("The available criteria includes: 'flight_id','origin_airport_id' ,'destination_airport_id', 'departure_date', 'departure_time','arrival_time', 'status', 'pilot_id")
+
+    criteria = input("Enter the criteria: ").strip()
+
+    if criteria not in flight_information:
+        print("The criteria is invalid") 
+        connect.close()
+        return
+
+    value = input(f"Enter the value for {criteria}: ").strip()
+    query = (f"SELECT * FROM flights where {criteria} = ?")
+    cursor.execute(query,(value,))
+    flights = cursor.fetchall()
+    connect.close()
+
+    if flights:
+        for flight in flights:
+            print(flight)
+    else:
+        print("No criteria was found")
+
+
 
 # This function create a command line interface
 def command_line_interface():
@@ -164,20 +210,23 @@ def command_line_interface():
         print("6. Show All Flights")
         print("7. Pilot Schedule")
         print("8. View/Update Destination Information")
+        print("9. View Flights by Criteria")
         print("10. Exit")
         
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice: ").strip()
         
         
         if choice == "1":
-            flight_id = int(input("Enter flight ID: "))
-            destination_id = int(input("Enter destination ID: "))
-            departure_date = input("Enter departure date (YYYY-MM-DD): ")
-            departure_time = input("Enter departure time (HH:MM): ")
+            #flight_id = int(input("Enter flight ID: ").strip())
+            origin_airport_id = int(input("Enter origin airport ID: ").strip())
+            destination_airport_id = int(input("Enter destination airport ID: ").strip())
+            departure_date = input("Enter departure date (YYYY-MM-DD): ".strip())
+            departure_time = input("Enter departure time (HH:MM): ".strip())
+            arrival_time = input("Enter arrival time (HH:MM): ".strip())
             status = input("Enter flight status: ")
-            pilot_id = int(input("Enter pilot ID: "))
-            add_flight(flight_id, destination_id, departure_date, departure_time, status, pilot_id)
-            print("Flight added successfully!")
+            pilot_id = int(input("Enter pilot ID: ").strip())
+            add_flight(origin_airport_id, destination_airport_id, departure_date, departure_time, arrival_time, status, pilot_id)
+            
         
         elif choice == "2":
             update_flight_information()
@@ -190,7 +239,7 @@ def command_line_interface():
         elif choice == "4":
             connect = connect_db()
             cursor = connect.cursor()
-            cursor.execute("SELECT * FROM destinations")
+            cursor.execute("SELECT * FROM airports")
             rows = cursor.fetchall()
             for row in rows:
                 print(row)
@@ -221,9 +270,12 @@ def command_line_interface():
 
         elif choice == "8":
             view_update_destination()
-        
+
         elif choice == "9":
-            print("Goodbye!")
+            flight_by_criteria()
+        
+        elif choice == "10":
+            print("Thank you")
             break
         
         else:
